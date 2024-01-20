39a551ae9aca7f8c262385b4aa90ef4a96745a48
Tyler Morton <tbmorton@ucsc.edu>
2024-01-20 14:16:45 -0800

    sam module is taking shape

--- backend/sam/create_model.py
+++ backend/sam/create_model.py
@@ -44,26 +44,43 @@ def embed_image(input_path: str, output_path: str):
     image = cv2.imread(input_path)
     mask_generator = SamAutomaticMaskGenerator(sam)
     masks = mask_generator.generate(image)
-    # save_mask(masks)
-
     predictor = SamPredictor(sam)
     predictor.set_image(image)
     image_embedding = predictor.get_image_embedding().cpu().numpy()
     np.save(output_path, image_embedding)
 
 
-def point_image(input_path, output_path):
+def save_mask_img(mask, ax, output):
+    print(mask.shape)
+    mask_image = mask_img(mask, ax, output)
+    im = Image.fromarray(mask_image)
+    im.save(output + ".jpeg")
+    ax.imshow(mask_image)
+
+
+def mask_img(mask):
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1)  # * color.reshape(1, 1, -1)
+    mask_image = np.squeeze(mask_image, axis=2)
+    return mask_image
+
+
+def show_mask(mask, ax, output):
+    # color = np.array([30 / 255, 144 / 255, 255 / 255, 0.6])
+    print(mask.shape)
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1)  # * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+def point_image(input_path, output_path, x, y):
     sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT)
     image = cv2.imread(input_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     mask_generator = SamAutomaticMaskGenerator(sam)
     # masks = mask_generator.generate(image)
     # save_mask(masks)
-
-    predictor = SamPredictor(sam)
-    predictor.set_image(image)
-    input_point = np.array([[500, 500]])
-    input_label = np.array([1])
+    # masks = mask_generator.generate(image)
     masks, scores, logits = predictor.predict(
         point_coords=input_point,
         point_labels=input_label,
@@ -74,80 +91,57 @@ def point_image(input_path, output_path):
     show_points(input_point, input_label, plt.gca())
     plt.axis("on")
     plt.show()
-    # image_embedding = predictor.get_image_embedding().cpu().numpy()
-    # np.save(output_path, image_embedding)
+
+    masks, scores, logits = predictor.predict(
+        point_coords=input_point,
+        point_labels=input_label,
+        multimask_output=True,
+    )
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+        plt.figure(figsize=(10, 10))
+        # plt.imshow(image)
+        show_mask(mask, plt.gca(), output_path)
+        show_points(input_point, input_label, plt.gca())
+        plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
+        plt.axis("off")
+        plt.show()
+        # image_embedding = predictor.get_image_embedding().cpu().numpy()
+        # np.save(output_path, image_embedding)
 
 
-# onnx_model_path = None
+def mask_query(img_path, point) -> []:
+    """
+    img_path - path to the image
+    point - cartesian coord query for the mask
+    returns - array of np-arrays representative of images (JPEG)
+    """
+    sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT)
+    image = cv2.imread(input_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    mask_generator = SamAutomaticMaskGenerator(sam)
+    predictor = SamPredictor(sam)
+    predictor.set_image(image)
+    input_point = np.array([[point.x, point.y]])
+    input_label = np.array([1])
+    masks, scores, logits = predictor.predict(
+        point_coords=input_point,
+        point_labels=input_label,
+        multimask_output=True,
+    )
+    mask_images = []
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+        # save_mask_img(mask, plt.gca(), output_path + "i")
+        mask_images.append(mask_img(mask))
+    return mask_images
 
-# import warnings
-
-# onnx_model_path = "sam_onnx_example.onnx"
-
-# onnx_model = SamOnnxModel(sam, return_single_mask=True)
-
-# dynamic_axes = {
-#     "point_coords": {1: "num_points"},
-#     "point_labels": {1: "num_points"},
-# }
-onnx_model_quantized_path = "sam_onnx_quantized_example.onnx"
-# quantize_dynamic(
-#     model_input=onnx_model_path,
-#     model_output=onnx_model_quantized_path,
-#     # optimize_model=True,
-#     per_channel=False,
-#     reduce_range=False,
-#     weight_type=QuantType.QUInt8,
-# )
-# onnx_model_path = onnx_model_quantized_path
-
-# ort_session = onnxruntime.InferenceSession(onnx_model_path)
-# embed_dim = sam.prompt_encoder.embed_dim
-# embed_size = sam.prompt_encoder.image_embedding_size
-# mask_input_size = [4 * x for x in embed_size]
-# dummy_inputs = {
-#     "image_embeddings": torch.randn(1, embed_dim, *embed_size, dtype=torch.float),
-#     "point_coords": torch.randint(low=0, high=1024, size=(1, 5, 2), dtype=torch.float),
-#     "point_labels": torch.randint(low=0, high=4, size=(1, 5), dtype=torch.float),
-#     "mask_input": torch.randn(1, 1, *mask_input_size, dtype=torch.float),
-#     "has_mask_input": torch.tensor([1], dtype=torch.float),
-#     "orig_im_size": torch.tensor([1500, 2250], dtype=torch.float),
-# }
-# output_names = ["masks", "iou_predictions", "low_res_masks"]
-
-# with warnings.catch_warnings():
-#     warnings.filterwarnings("ignore", category=torch.jit.TracerWarning)
-#     warnings.filterwarnings("ignore", category=UserWarning)
-#     with open(onnx_model_path, "wb") as f:
-#         torch.onnx.export(
-#             onnx_model,
-#             tuple(dummy_inputs.values()),
-#             f,
-#             export_params=True,
-#             verbose=False,
-#             opset_version=17,
-#             do_constant_folding=True,
-#             input_names=list(dummy_inputs.keys()),
-#             output_names=output_names,
-#             dynamic_axes=dynamic_axes,
-#         )
-
-
-# onnx_model_quantized_path = "sam_onnx_quantized_example.onnx"
-# quantize_dynamic(
-#     model_input=onnx_model_path,
-#     model_output=onnx_model_quantized_path,
-#     # optimize_model=True,
-#     per_channel=False,
-#     reduce_range=False,
-#     weight_type=QuantType.QUInt8,
-# )
-# onnx_model_path = onnx_model_quantized_path
-
-# ort_session = onnxruntime.InferenceSession(onnx_model_path)
 
 if __name__ == "__main__":
     input_path = sys.argv[1]
     output_path = sys.argv[2]
-    embed_image(input_path, output_path)
-    # point_image(input_path, output_path)
+
+    embed_image(input_path, output_path)  # for embedding images
+
+    # uncomment if you want to get only a point image
+    # x = sys.argv[3]
+    # y = sys.argv[4]
+    # point_image(input_path, output_path, x, y)
