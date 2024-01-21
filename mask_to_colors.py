943f30d763000e60a71ce2014ed987511f50c95b
Sebastian Osorio <bearcbass@gmail.com>
2024-01-20 21:20:03 -0800

    Updated the file to add more values

--- mask_to_colors.py
+++ mask_to_colors.py
@@ -1,16 +1,19 @@
 import cv2
 import numpy as np
 
-def create_hsv_ranges():
-    # Define HSV color ranges for various colors
+def create_rgb_ranges():
+    # Define RGB color ranges for various colors
     color_ranges = {
-        'red': ([0, 100, 100], [10, 255, 255]),
+        'red': ([0, 100, 100], [250, 255, 255]),
         'blue': ([110, 100, 100], [130, 255, 255]),
         'yellow': ([20, 100, 100], [30, 255, 255]),
         'brown': ([0, 60, 60], [20, 255, 255]),
         'violet': ([130, 100, 100], [160, 255, 255]),
         'orange': ([10, 100, 100], [20, 255, 255]),
         'green': ([40, 100, 100], [80, 255, 255]),
+        'white': ([255,255,255],[225,225,225]),
+        'black': ([0,0,0],[30,30,30]),
+        'grey': ([31,31,31],[])
     }
 
     return color_ranges
@@ -27,18 +30,26 @@ def create_masked_object(original_image, mask_image):
 
     return masked_object
 
-
 result_image = cv2.imread('result_image.jpg')
+
+hsv_image = cv2.cvtColor(result_image, cv2.COLOR_BGR2HSV)
+
+lab_image = cv2.cvtColor(result_image, cv2.COLOR_BGR2LAB)
 
 mask_image = cv2.imread('greenband.jpeg', cv2.IMREAD_GRAYSCALE)
 
 cv2.imshow('Original Image', result_image)
+cv2.imshow('lab Image', lab_image)
 cv2.imshow('mask Image', mask_image)
+cv2.imshow('hsv Image', hsv_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 new_image = create_masked_object(result_image, mask_image)
 
+hsv_update = cv2.cvtColor(new_image, cv2.COLOR_BGR2HSV)
+
+cv2.imshow('cool', hsv_update)
 cv2.imshow('new ', new_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
@@ -61,10 +72,17 @@ average_per_column = np.mean(sample_points, axis=0)
 cv2.imshow('Result Image', result_image)
 cv2.imshow('Mask Image', mask_image)
 cv2.imshow('New Image', new_image)
-print("Average RGB Value per Column:\n", average_per_column)
 
 # Create vectors with average RGB values and sampled RGB values
-average_rgb_vector = np.round(average_per_column).astype(int)
+average_rgb_vector = np.round(average_per_column).astype(np.uint8)
+print("Average RGB Value per Column:\n", average_rgb_vector)
+
+#This makes cvtColor work
+average_rgb_vector = average_rgb_vector.reshape(1, 1, 3)
+print("this is the rgb color: ", average_rgb_vector)
+
+hsv_color = cv2.cvtColor(average_rgb_vector, cv2.COLOR_RGB2HSV)
+print("this is the hsv color",hsv_color)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
